9cb7809ea62a82d9bc71287f506978896c2483d0
“polygoncoin <polygon.co.in@gmail.com>
2025-07-17 00:22:16 +0530

    Refactoring changes

--- Autoload.php
+++ Autoload.php
@@ -8,7 +8,7 @@
  * @author    Ramesh N Jangid <polygon.co.in@gmail.com>
  * @copyright 2025 Ramesh N Jangid
  * @license   MIT https://opensource.org/license/mit
- * @link      https://github.com/polygoncoin/Microservices
+ * @link      https://github.com/polygoncoin/Heavy-Objects
  * @since     Class available since Release 1.0.0
  */
 namespace HeavyObjects;
@@ -22,7 +22,7 @@ namespace HeavyObjects;
  * @author    Ramesh N Jangid <polygon.co.in@gmail.com>
  * @copyright 2025 Ramesh N Jangid
  * @license   MIT https://opensource.org/license/mit
- * @link      https://github.com/polygoncoin/Microservices
+ * @link      https://github.com/polygoncoin/Heavy-Objects
  * @since     Class available since Release 1.0.0
  */
 class Autoload
